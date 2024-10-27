4d8d784a8e19d4f0911a09a4695bfbb28f2d62a2
Dante4K18 <dan@dan>
2024-10-27 18:40:51 +0300

    complete-dsa-tree-data-structure

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,14 @@
 class Tree:
-  def __init__(self, root = None):
+  def __init__(self, root):
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    stack = [self.root]
+
+    while stack:
+      node = stack.pop()
+      if node.get('id') == id:
+        return node
+      stack.extend(node.get('children', []))
+
+    return None
